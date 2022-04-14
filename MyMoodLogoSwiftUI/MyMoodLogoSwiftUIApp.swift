e5a0749a82dc885610f5a2c5d392af7a6ac8fc6c
Alexey <alexeyradomskiy98@gmail.com>
2022-04-14 19:44:54 +0300

    Initial Commit

---
+++ MyMoodLogoSwiftUI/MyMoodLogoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoodLogoSwiftUIApp.swift
+//  MyMoodLogoSwiftUI
+//
+//  Created by Alexey on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMoodLogoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
